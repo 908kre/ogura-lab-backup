fd45f0bd3a2592bb81fb8ccdbcbdb501c5823750
x1nyuan <yao.ntno@gmail.com>
2018-10-07 15:01:40 +0900

    tmp

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const { VueLoaderPlugin } = require("vue-loader");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 
 const NODE_ENV = process.env.NODE_ENV;
 
@@ -8,44 +10,79 @@ const config = {
   mode: NODE_ENV,
   entry: './src/main.js',
   output: {
-    filename: '[name][hash:7].js',
+    filename: '[name].[hash:7].js',
   },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
+      template: './public/index.html',
+    }),
   ],
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        uglifyOptions: {
+          mangle:{
+            keep_fnames: true
+          }
+        }
+      }),
+    ]
+  },
   module: {
     rules: [
       {
-        test: /\.(css|sass|scss)$/,
+        test: /\.(css|scss|sass)$/,
         use: [
           'vue-style-loader',
           'css-loader',
-          'sass-loader'
+          'sass-loader',
         ],
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: 'vue-loader',
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
         options: {
           presets: [
-            ['env', {'modules': false}]
-          ]
+            [
+              "@babel/env", 
+              {
+                modules: false,
+                targets: {
+                  "browsers": ["> 1%", "last 2 versions", "not ie <= 8"]
+                }
+              }
+            ],
+          ],
         },
         exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: '[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: '[name].[hash:7].[ext]'
+        }
       },
     ]
   },
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': path.join(__dirname, 'src'),
+      '@': '/srv/src',
     },
     extensions: ['*', '.js', '.vue', '.json']
   }
